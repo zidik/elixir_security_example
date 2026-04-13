a317fb51ecdf7fda6698005482cc7053c1e8e18b
Mark Laane <1482651+zidik@users.noreply.github.com>
2026-04-14 00:22:32 +0300

    disable submission, add plug

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule ElixirSecurityExample.MixProject do
   defp deps do
     [
       {:hackney, "~> 1.17.0"},
-      {:ecto, "== 2.2.0"}
+      {:ecto, "== 2.2.0"},
+      {:plug, "1.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
